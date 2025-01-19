3555f164b5df917e2909929bcb651b168d08ed79
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2025-01-19 12:20:39 +0530

    Update db_config.php

--- models/db_config.php
+++ models/db_config.php
@@ -1,15 +1,4 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "blog app";
-
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
 
 $servername = "localhost";  // Database server (localhost for local development)
 $username = "root";         // MySQL username
@@ -22,7 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);  // If the connection fails, show error
 }
-
-
 
 ?>
